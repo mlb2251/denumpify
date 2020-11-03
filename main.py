3ba3e99b1f2f53162ea410bd7267700e2d237c5b
Matt Bowers <mattb6503@gmail.com>
2020-11-03 13:53:50 -0500

    changed Expr internals

--- main.py
+++ main.py
@@ -59,6 +59,8 @@ def get_numpy_fns():
 
     # ignore ones starting with underscore
     fns = [f for f in fns if not f.__name__.startswith('_')]
+    reject = {'lookfor', 'info', 'source', 'printoptions', 'set_printoptions', }
+    fns = [f for f in fns if f.__name__ not in reject]
 
     return fns
 
@@ -164,25 +166,52 @@ def synth(fns, cfg):
     names = [f.__name__ for f in fns]
     prims = []
     for name,fn in zip(names,fns):
-        prims.append(Prim(name,fn,TFunc(1)))
+        prims.append(PrimFunc(name,fn,TFunc(1)))
 
-    prims += [Prim(str(i), Const(i), TVal()) for i in range(-1,2)]
-    prims += [Prim(name, Var(name), TVal()) for name in ('x','y')]
+    prims += [PrimConst(str(i), i, TVal()) for i in range(-1,2)]
+    prims += [PrimVar(name, name, TVal()) for name in ('x','y')]
 
     funcs = {p for p in prims if p.is_func}
-    non_funcs = {p for p in prims if not p.is_func}
+    exprs = {p() for p in prims if not p.is_func}
+    seen = set()
+
+    env = {
+        'x':np.ones((2,3)),
+        'y':0,
+    }
 
     print("bottom upping")
     # bottom up
-    for _ in range(1):
+    for _ in range(2):
         to_add = set()
         for f in funcs:
-            for nf in non_funcs:
-                to_add.add(f(nf))
-                print(len(to_add))
-        non_funcs |= to_add
-        
+            for e in exprs:
+                new = f(e)
+                if str(new) in seen:
+                    continue
+                seen.add(str(new))
+                res = new.eval(env)
+                if isinstance(res,ProgramException):
+                    mlb.red(f'{new} -> error: {res}')
+                    continue
+                try:
+                    s = str(res)
+                except TypeError:
+                    s = "[unable to print result]"
+                    mlb.red(f'{new} -> error: {e}')
+                    continue
 
+                mlb.green(f'{e} -> {s}')
+
+                to_add.add(new)
+                print(len(to_add))
+        exprs |= to_add
+    
+    by_depth = defaultdict(list)
+    for e in exprs:
+        by_depth[e.depth].append(e)
+
+    print({k:len(v) for k,v in by_depth.items()})
 
     return
 
--- program.py
+++ program.py
@@ -1,55 +1,122 @@
 
 
 class Expr:
-    pass
+    def __init__(self,from_prim=None):
+        self.prim = from_prim
+        self._eval_envs = []
+        self._eval_vals = []
+        self._saved_size = None
+        self._saved_depth = None
+        self.last_eval = None
+    def eval(self, env:dict):
+        if env in self._eval_envs:
+            return self._eval_vals[self._eval_envs.index(env)]
+        res = self._eval(env)
+        self._eval_envs.append(env)
+        self._eval_vals.append(res)
+        self.last_eval = res
+        return res
+    def _eval(self, env:dict):
+        raise NotImplementedError
+    @property
+    def size(self):
+        if self._saved_size is None:
+            self._saved_size = self._size()
+        return self._saved_size
+    def _size(self):
+        raise NotImplementedError
+    @property
+    def depth(self):
+        if self._saved_depth is None:
+            self._saved_depth = self._depth()
+        return self._saved_depth
+    def _depth(self):
+        raise NotImplementedError
+        
+
+class ProgramException(Exception): pass
 
 class App(Expr):
-    def __init__(self, fn, args):
-        self.fn = fn
+    def __init__(self, fn_prim, args, **kwargs):
+        super().__init__(**kwargs)
+        assert fn_prim.is_func
+        assert len(args) == fn_prim.tp.argc
+        assert all([isinstance(arg, Expr) for arg in args])
+
+        self.fn = fn_prim.val
+        self.tp = fn_prim.tp
+        self.name = fn_prim.name
         self.args = args
 
-        assert self.fn.is_func
-        assert len(self.args) == self.fn.tp.argc
-    def eval(self,env:dict):
-        self.fn.val([arg.eval(env) for arg in self.args])
+    def _eval(self,env:dict):
+        args = [arg.eval(env) for arg in self.args]
+        try:
+            return self.fn(args)
+        except Exception as e:
+            return ProgramException(e)
     def __repr__(self):
         args = ','.join([repr(arg) for arg in self.args])
-        return  f'{self.fn}({args})'
+        return  f'{self.name}({args})'
+    def _size(self):
+        return 1 + sum([arg.size for arg in self.args])
+    def _depth(self):
+        return 1 + max([arg.depth for arg in self.args])
+
 
 
 class Var(Expr):
-    def __init__(self, name:str):
+    def __init__(self, name:str, **kwargs):
+        super().__init__(**kwargs)
         self.name = name
-    def eval(self,env:dict):
-        return env[self.name]
+    def _eval(self,env:dict):
+        try:
+            return env[self.name]
+        except KeyError as e:
+            return ProgramException(e)
     def __repr__(self):
         return self.name
+    def _size(self):
+        return 1
+    def _depth(self):
+        return 1
 
 class Const(Expr):
-    def __init__(self,val):
+    def __init__(self, val, **kwargs):
+        super().__init__(**kwargs)
         self.val = val
-    def eval(self,env:dict):
+    def _eval(self,env:dict):
         return self.val
     def __repr__(self):
         return repr(self.val)
+    def _size(self):
+        return 1
+    def _depth(self):
+        return 1
+
 
 class Prim:
     def __init__(self, name, val, tp):
         self.name = name
         self.val = val
         self.tp = tp
-
+        self.id = name
     
     @property
     def is_func(self):
-        return isinstance(self.tp,TFunc)
+        return isinstance(self,PrimFunc)
 
-    def __call__(self, *args):
-        if self.is_func:
-            return App(self, args)
-        return Const(self)
     def __repr__(self):
         return self.name
+
+class PrimConst(Prim):
+    def __call__(self):
+        return Const(self.val, from_prim=self)
+class PrimVar(Prim):
+    def __call__(self):
+        return Var(self.val, from_prim=self)
+class PrimFunc(Prim):
+    def __call__(self, *args):
+        return App(self, args, from_prim=self)
 
 
 class Type:
